282effe831426b41f81d2fac63251238c1bc2083
aliendr <35575534+aliendr@users.noreply.github.com>
2020-04-15 14:52:46 +0300

    webhook locally tested

---
+++ src/main/java/com/example/trello/services/WebhookService.java
@@ -0,0 +1,14 @@
+package com.example.trello.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WebhookService {
+
+
+
+
+    public void validateWebhook(String request){
+
+    }
+}
